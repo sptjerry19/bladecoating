32cc8208dc62af6da9052f874e0802d0c51e57d2
sptjerry19 <duylinhvnu@gmail.com>
2024-11-14 11:46:30 +0700

    feat touch sensor

--- arduino/lcd/lcd.ino
+++ arduino/lcd/lcd.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+#define BUTTON_PIN 4 
+struct touch { 
+	 byte wasPressed = LOW; 
+	 byte isPressed = LOW; 
+}; 
+touch touch; 
+void setup() 
+{ 
+	 pinMode(BUTTON_PIN, INPUT); 
+	 Serial.begin(115200); 
+} 
+void loop() 
+{ 
+	 touch.isPressed = isTouchPressed(BUTTON_PIN); 
+	 if (touch.wasPressed != touch.isPressed) { 
+	   Serial.println("Touch pressed"); 
+	 } 
+	 touch.wasPressed = touch.isPressed; 
+} 
+bool isTouchPressed(int pin) 
+{ 
+	 return digitalRead(pin) == HIGH; 
+} 
